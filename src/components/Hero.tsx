b31070a047bf9df9bb448a781a8b714917b2c1e3
Shawon <samsul261103@gmail.com>
2026-09-11 19:17:07 +0600

    fix hero section 2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import heroPhoto from "../assets/banner-stack.png";
 
 const Hero = () => {
   return (
-    <div className="flex items-center justify-evenly gap-6 w-[80%] mx-auto">
+    <div className="flex items-center justify-between gap-6 w-[80%] mx-auto">
       <div className="max-w-xl">
         <h1 className="text-6xl py-3 font-bold ">Build Your Ideal</h1>
         <h1 className="text-6xl py-3 font-bold bg-linear-to-r from-orange-500 via-pink-500 to-violet-500 bg-clip-text text-transparent">
