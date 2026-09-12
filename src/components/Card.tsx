a8af8eb60c13242317c24b01241c785ffc13103a
Shawon <samsul261103@gmail.com>
2026-09-12 16:45:18 +0600

    added Your stack section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,41 +1,47 @@
 // import React from 'react';
+import type { Technology } from "./type/Type";
 
-interface Technology {
-  id: number;
-  name: string;
-  category: string;
-  description: string;
-  icon: string;
-  rating: number;
-  difficulty: string;
-  badge: string;
-}
-
-
-const Card = ({ technology }: { technology: Technology }) => {
+const Card = ({
+  technology,
+  onAdd,
+  isSelected,
+}: {
+  technology: Technology;
+  onAdd: (technology: Technology) => void;
+  isSelected: boolean;
+}) => {
   return (
-     <div className="border-2 border-blue-100 rounded-xl p-4">
-        <div className="flex justify-end ">
-            <div className="w-fit rounded-4xl px-3 py-1 bg-blue-100 text-blue-400 border-2 border-blue-200">
-            {technology.badge}
-        </div>
-        </div>
-
-        <img src={technology.icon} alt={technology.name} className="w-10 h-10" />
-
-        <h2 className="font-bold text-xl my-4">{technology.name}</h2>
-        <p className="text-gray-500 text-sm py-3">
-          {technology.description}
-        </p>
-        <div className="flex justify-between gap-5 py-4">
-          <p className="bg-gray-200 px-2 py-1 rounded-xl text-sm">{technology.category}</p>
-          <p className="text-sm">{technology.difficulty}</p>
-          <p>⭐ {technology.rating}</p>
-        </div>
-        <div className="">
-          <button className="btn btn-neutral w-full rounded-xl">Add to Stack</button>
+    <div className="border-2 border-blue-100 rounded-xl p-4">
+      <div className="flex justify-end ">
+        <div className="w-fit rounded-4xl px-3 py-1 bg-blue-100 text-blue-400 border-2 border-blue-200">
+          {technology.badge}
         </div>
       </div>
+
+      <img src={technology.icon} alt={technology.name} className="w-10 h-10" />
+
+      <h2 className="font-bold text-xl my-4">{technology.name}</h2>
+      <p className="text-gray-500 text-sm py-3">{technology.description}</p>
+      <div className="flex justify-between gap-5 py-4">
+        <p className="bg-gray-200 px-2 py-1 rounded-xl text-sm">
+          {technology.category}
+        </p>
+        <p className="text-sm">{technology.difficulty}</p>
+        <p>⭐ {technology.rating}</p>
+      </div>
+      <div className="">
+        <button
+          onClick={() => onAdd(technology)}
+          disabled={isSelected}
+          className={`btn w-full rounded-xl ${
+            isSelected
+              ? "bg-pink-100 text-pink-500 border-pink-200"
+              : "btn-neutral"
+          }`}>
+          {isSelected ? "✓ Added to Stack":"Add to Stack"}
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,37 +1,38 @@
 // import React from 'react';
 
-import { use } from "react";
+import { use, useState } from "react";
 import Card from "./Card";
+import type { Technology } from "./type/Type";
 
-interface Technology {
-  id: number;
-  name: string;
-  category: string;
-  description: string;
-  icon: string;
-  rating: number;
-  difficulty: string;
-  badge: string;
-}
-
-const getTechnologies= async()=>{
-  const res= await fetch("/technologies.json");
-  const data= await res.json();
+const getTechnologies = async (): Promise<Technology[]> => {
+  const res = await fetch("/technologies.json");
+  const data = await res.json();
   return data;
-}
+};
 
-const technologyPromise=getTechnologies()
-
+const technologyPromise = getTechnologies();
 
 const Technologies = () => {
-  const technologies =use(technologyPromise);
+  const technologies = use(technologyPromise);
+
+  const [selectedTechnologies, setSelectedTechnologies] = useState<
+    Technology[]
+  >([]);
+
+  const handleAddTechnology = (technology: Technology) => {
+    console.log("Clicked:", technology);
+    setSelectedTechnologies([...selectedTechnologies, technology]);
+  };
+
   return (
     <div>
       <div className="w-[80%] mx-auto">
         <h1 className="text-4xl font-bold py-4">
           Explore the <span className="text-pink-500">Technologies</span>
         </h1>
-        <p className="pb-6 text-gray-600">Pick one technology per category to build your ideal stack.</p>
+        <p className="pb-6 text-gray-600">
+          Pick one technology per category to build your ideal stack.
+        </p>
       </div>
 
       {/* card section */}
@@ -39,31 +40,64 @@ const Technologies = () => {
         {/* Tech Card  */}
         <div className="col-span-3">
           <div className="grid grid-cols-3 gap-5">
-                {
-                  technologies.map((technology:Technology)=>(
-                    <Card key={technology.id} technology={technology}/>
-                  ))
-                }
+            {technologies.map((technology) => (
+              <Card
+                key={technology.id}
+                technology={technology}
+                onAdd={handleAddTechnology}
+                isSelected={selectedTechnologies.some(
+                  (item) => item.id === technology.id,
+                )}
+              />
+            ))}
           </div>
         </div>
 
-
-
-
-
-
-
-
-
-
-
         {/* Select Card  */}
         <div className="col-span-1 border-2 border-gray-300 px-6 py-4 self-start">
-            <h2 className="text-2xl font-bold py-2">Your Stack</h2>
-            <p className="text-gray-400 py-2">No technologies selected yet.</p>
-            <div className="border-2 border-dashed rounded-2xl border-gray-300">
-              <p className="text-center text-gray-400 p-10">Your stack is empty</p>
-            </div>
+          <h2 className="text-2xl font-bold py-2">Your Stack</h2>
+          <p className="text-gray-400 py-2">
+            {selectedTechnologies.length===0 ? "No technologies selected yet." : `${selectedTechnologies.length} technology selected`}
+          </p>
+
+          <div className="space-y-3">
+            {selectedTechnologies.length === 0 ? (
+              <div className="border-2 border-dashed rounded-2xl border-gray-300">
+                <p className="text-center text-gray-400 p-10">
+                  Your stack is empty
+                </p>
+              </div>
+            ) : (
+              selectedTechnologies.map((technologies) => (
+                <div
+                  key={technologies.id}
+                  className="border border-gray-200 rounded-xl flex items-center justify-between "
+                >
+                  <div className="flex items-center gap-3 m-3">
+                    <img
+                      src={technologies.icon}
+                      alt={technologies.name}
+                      className="w-10 h-10"
+                    />
+
+                    <div>
+                      <h3 className="font-semibold">{technologies.name}</h3>
+                      <p className="text-sm text-gray-500">
+                        {technologies.category}
+                      </p>
+                    </div>
+                  </div>
+                  <button className="pr-3 text-gray-400 text-3xl">✕</button>
+                </div>
+              ))
+            )}
+          </div>
+
+          {selectedTechnologies.length > 0 && (
+            <button className="btn w-full mt-4 rounded-xl border border-red-300 text-red-500 bg-white">
+              Remove All
+            </button>
+          )}
         </div>
       </div>
     </div>
