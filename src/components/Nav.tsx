1a31824b475aacfbead9464961f1831bad282d32
Shawon <samsul261103@gmail.com>
2026-09-13 02:41:40 +0600

    responsive nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,19 @@
 // import React from "react";
 import logo from "../assets//logo-text.png";
+import { FaBars } from "react-icons/fa";
 const Nav = () => {
   return (
     <div className="flex items-center justify-around sticky top-0 bg-white py-5">
+
+    <button className="md:hidden text-3xl">
+      <FaBars/>
+    </button>
+
       {/* left side image */}
-      <img src={logo} alt="Dev Stack" />
+      <img src={logo} alt="Dev Stack"  className=""/>
 
       {/* middle a nav item */}
-      <div className="flex gap-6 text-gray-600">
+      <div className="hidden md:flex gap-6 text-gray-600 ">
         <a href="#" className="text-pink-500 font-semibold">Home</a>
         <a href="#">Technologies</a>
         <a href="#">Projects</a>
@@ -17,7 +23,7 @@ const Nav = () => {
 
       {/* right sign in/up button */}
 
-      <div className="flex gap-2">
+      <div className="flex gap-2 sm:">
         <button className="btn bg-white border-0 text-gray-600 rounded-4xl px-7">Sign In</button>
         
         <button className="btn bg-pink-600 rounded-4xl px-7 text-white">Sign Up</button>
