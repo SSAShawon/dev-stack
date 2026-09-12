aaebfdce9197cce3350f1e23ce59221199fa5dfb
Shawon <samsul261103@gmail.com>
2026-09-12 23:29:31 +0600

    add suspense

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import Technologies from './components/Technologies';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Suspense } from 'react';
 
 
 const App = () => {
@@ -14,7 +15,16 @@ const App = () => {
       <Hero/>
       <br />
       <br />
-      <Technologies/>
+
+      <Suspense fallback={
+        <div className='text-2xl text-center font-bold'>
+          <h1>Loading...</h1>
+          <h1> Please wait</h1>
+        </div>
+      }>
+        <Technologies/>
+      </Suspense>
+      
       <br />
       <br />
        <div className="divider"></div>
